9189c631da4fbebe54a980f9143de2c72cdf6756
a d <84557399+akhmed-codes@users.noreply.github.com>
2021-08-05 12:57:00 +0530

    Delete quiz-game/target/debug/build directory

--- quiz-game/target/debug/build/libc-3d8200197577bc6f/build_script_build-3d8200197577bc6f.d
+++
@@ -1,5 +0,0 @@
-/home/shivam/quiz-game/target/debug/build/libc-3d8200197577bc6f/build_script_build-3d8200197577bc6f: /home/shivam/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.98/build.rs
-
-/home/shivam/quiz-game/target/debug/build/libc-3d8200197577bc6f/build_script_build-3d8200197577bc6f.d: /home/shivam/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.98/build.rs
-
-/home/shivam/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.98/build.rs:
